42b9b6f4e9b0e1073c5db5a476637a3bc8e0cf72
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-08-06 14:20:23 -0500

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'brint.ohearn@rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures Magento'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 depends 'magento', '>= 0.8.5'
 depends 'php-fpm'
