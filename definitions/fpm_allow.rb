fdad3e9b12070031d8d819149beb7ec90624d000
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-08-07 14:32:53 -0500

    Adding fpm_allow for multi web server setups

---
+++ definitions/fpm_allow.rb
@@ -0,0 +1,55 @@
+define :fpm_allow do
+
+  php_conf =  if platform?('centos', 'redhat')
+                ["/etc", "/etc/php.d"]
+              else
+                ["/etc/php5/fpm", "/etc/php5/conf.d"]
+              end
+
+  if node['rax']['php-fpm']['slaves'].empty?
+  	permit = ",#{node['rax']['php-fpm']['master']}"
+  else
+    permit = ",#{node['rax']['php-fpm']['master']},#{node['rax']['php-fpm']['slaves'].join(",")}"
+  end
+
+  node['php-fpm']['pools'].each do |pool|
+    bash "Permit slave nodes of #{pool} to leverage this PHP-FPM setup" do
+      cwd "#{php_conf[0]}/pools/" # php.ini location
+      code <<-EOH
+      sed -i 's/listen.allowed_clients = .*/listen.allowed_clients = 127.0.0.1#{permit}/' #{pool}.conf
+      EOH
+      notifies :restart, resources(:service => "php-fpm")
+    end
+  end
+
+  # Setup firewall rules
+  fpm_port = node['php-fpm']['pool']['magento']['listen'].split(":")[1].to_i
+
+  case node["platform_family"]
+  when "rhel", "fedora"
+    fwfile = "/etc/sysconfig/iptables"
+
+    node['rax']['php-fpm']['slaves'].each do |ip|
+      rule = "-I INPUT -s #{ip} -p tcp -m tcp --dport #{fpm_port} -j ACCEPT"
+      execute "Adding iptables rule for #{port}" do
+        command "iptables #{rule}"
+        not_if "grep \"\\#{rule}\" #{fwfile}"
+      end
+    end
+    # Save iptables rules
+    execute "Saving fpm iptables rule set" do
+      command "/sbin/service iptables save"
+    end
+  else
+    include_recipe "firewall"
+
+    node['rax']['php-fpm']['slaves'].each do |ip|
+      firewall_rule "fpm-#{fpm_port}" do
+        port fpm_port
+        source ip
+        interface "eth1"
+        action :allow
+      end
+    end
+  end
+end
--- recipes/php_fpm.rb
+++ recipes/php_fpm.rb
@@ -15,3 +15,8 @@ file File.join(node['php-fpm']['pool_conf_dir'], 'www.conf') do
   action :delete
   notifies :restart, "service[php-fpm]", :immediately
 end
+
+# The following should only be used in the case where there are multiple web
+# servers, and the master node needs to process admin-type requests. The
+# fpm_allow definition will allow other nodes to communicate directly with FPM.
+fpm_allow if node[:rax][:magento][:master]
