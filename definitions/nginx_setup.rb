5c9a656dae8362804fac8224b9a41fc584977800
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-08-11 18:07:04 -0500

    Overriding default nginx template

--- definitions/nginx_setup.rb
+++ definitions/nginx_setup.rb
@@ -18,4 +18,25 @@ define :nginx_setup do
     action :create_if_missing
     notifies :reload, "service[nginx]"
   end
+
+  %w(default ssl).each do |site|
+    template File.join(node[:nginx][:dir], 'sites-available', site) do
+      cookbook 'rax-magento'
+      source 'nginx-site.erb'
+      owner 'root'
+      group 'root'
+      mode 0644
+      variables(
+        path: node[:magento][:dir],
+        ssl: (site == 'ssl') ? true : false,
+        ssl_cert: File.join(node[:nginx][:dir], 'ssl',
+                            node[:magento][:cert_name]),
+        ssl_key: File.join(node[:nginx][:dir], 'ssl', node[:magento][:cert_name])
+      )
+    end
+    nginx_site site do
+      template nil
+      notifies :reload, resources(service: 'nginx')
+    end
+  end
 end
