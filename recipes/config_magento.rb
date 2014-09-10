3770f12b3273893af1a0dcb04477437a02f740b9
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-09-10 15:21:25 -0500

    Removing existing local.xml prior to running setup

--- recipes/config_magento.rb
+++ recipes/config_magento.rb
@@ -1,6 +1,10 @@
 # Encoding: utf-8
 
 unless File.exist?(File.join(node[:magento][:dir], '.configured'))
+  execute "Nuke local.xml for initial setup" do
+    command "rm -f #{File.join(node[:magento][:dir], 'app/etc/local.xml')}"
+    action :run
+  end
   node.set_unless[:rax][:magento][:encryption_key] = Magento.magento_encryption_key
   magento_initial_configuration
 end
